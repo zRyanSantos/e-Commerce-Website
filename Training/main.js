9947e86761d325aa85c56dad917ef1754cc354a9
RyanZito <ryanthiago.dsantos@gmail.com>
2024-01-04 22:40:19 -0300

    v1.3

--- Training/main.js
+++ Training/main.js
@@ -1,20 +1,20 @@
-var cm;
+// var cm;
 
-function ryanpenis(){
-    cm = prompt('QUAL O TAMANHO DO SEU INSTRUMENTO?')
-}
+// function ryanpenis(){
+//     cm = prompt('QUAL O TAMANHO DO SEU INSTRUMENTO?')
+// }
 
-ryanpenis()
-if(cm <= 5){
-    alert('VOCE TEM PIRU PEQUENO IGUAL O VININHO DO ROLAMENTO INTENSIVO')
-}
-else if(cm > 5 && cm <= 20){
-    alert('SEU PAU ESTÁ NA MÉDIA')
-}
-else if (cm > 20){
-    alert('SEU PAU É GIGANTESCO IGUAL O DO RYANZITO')
-}
-else{
-    alert(`Você digitou "${cm}", digite apenas numeros, não seja um neandertal!`)
-    ryanpenis()
-}
+// ryanpenis()
+// if(cm <= 5){
+//     alert('VOCE TEM PIRU PEQUENO IGUAL O VININHO DO ROLAMENTO INTENSIVO')
+// }
+// else if(cm > 5 && cm <= 20){
+//     alert('SEU PAU ESTÁ NA MÉDIA')
+// }
+// else if (cm > 20){
+//     alert('SEU PAU É GIGANTESCO IGUAL O DO RYANZITO')
+// }
+// else{
+//     alert(`Você digitou "${cm}", digite apenas numeros, não seja um neandertal!`)
+//     ryanpenis()
+// }
