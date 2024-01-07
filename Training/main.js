ea1236cd2adf77627a8f98a50dd2f2b85b15a8c2
RyanZito <ryanthiago.dsantos@gmail.com>
2024-01-07 01:22:46 -0300

    v1.0

--- Training/main.js
+++ Training/main.js
@@ -1,23 +1,44 @@
-var cm;
+// var cm;
 
-function calcIdade(){
-    cm = prompt('Qual é a sua idade?')
-}
+// function calcIdade(){
+//     cm = prompt('Qual é a sua idade?')
+// }
 
-calcIdade()
-if(cm < 10){
-    alert('Você ainda é bem jovem.')
-}
-else if(cm >= 10 && cm <= 14){
-    alert('Você é um pré-adolescente.')
-}
-else if (cm > 14 && cm < 18){
-    alert('Você é um adolescente.')
-}
-else if (cm >= 18){
-    alert('Você é um adulto.')
-}
-else{
-    alert(`Você digitou "${cm}". Digite apenas numeros para ter o resultado esperado!`)
-    calcIdade()
+// calcIdade()
+// if(cm > 0 && cm < 10){
+//     alert('Você ainda é bem jovem.')
+// }
+// else if(cm >= 10 && cm <= 14){
+//     alert('Você é um pré-adolescente.')
+// }
+// else if (cm > 14 && cm < 18){
+//     alert('Você é um adolescente.')
+// }
+// else if (cm >= 18){
+//     alert('Você é um adulto.')
+// }
+// else if (cm === null){
+// }
+// else{
+//     alert(`Você digitou "${cm}". Digite apenas numeros para ter o resultado esperado!`)
+//     calcIdade()
+// }
+
+var paisInput = document.querySelector('input#country')
+
+var response = document.querySelector('div.resposta')
+
+function calc() {
+
+    var pais = paisInput.value
+
+    response.innerHTML = `Vivendo em ${pais}<br>`
+
+    if (pais == 'Brasil'){
+        response.innerHTML += `Você brasileiro!`
+    }
+    else {
+        response.innerHTML += `Você é estrangeiro`
+    }
+
 }
