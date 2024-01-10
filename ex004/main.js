fe8ede7d6d0f21f6151ca172e0457204034addd7
RyanZito <ryanthiago.dsantos@gmail.com>
2024-01-10 00:16:38 -0300

    att

---
+++ ex004/main.js
@@ -0,0 +1,23 @@
+function tabuada() {
+
+    var verificar = document.querySelector('input#inicio')
+    var select = document.querySelector('select#sel')
+
+    select.innerHTML = ''
+
+if(verificar.value.length == 0){
+    alert('Por favor digite um número!')
+}
+else {
+    
+    var usernum = Number(verificar.value)
+
+    for ( i=1; i <= 10; i++ ) {
+
+        var tab = document.createElement('option')
+        tab.text = (`${usernum} X ${i} = ${usernum*i}`)
+        select.appendChild(tab)
+
+    }
+}
+}
