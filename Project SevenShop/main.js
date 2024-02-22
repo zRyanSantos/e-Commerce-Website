f00807da95971679a470902b58c8622109cb9450
RyanZito <ryanthiago.dsantos@gmail.com>
2024-02-22 02:03:28 -0300

    Change Theme v1.2

--- Project SevenShop/main.js
+++ Project SevenShop/main.js
@@ -1,14 +1,20 @@
 var body = document.body
 var button = document.querySelector('input.toggle')
 var container = document.querySelector('section.container')
+var header = document.querySelector('header.header')
+var products = document.querySelector('ol.products')
 
 button.addEventListener("click", function(){
     if(button.checked){
         body.classList = 'body_whiteTheme'
         container.classList = 'container_whiteTheme'
+        header.classList = 'header_whiteTheme'
+        products.classList = 'products_whiteTheme'
     }
     else{
         body.classList = 'body'
         container.classList = 'container'
+        header.classList = 'header'
+        products.classList = 'products'
     }
 })
