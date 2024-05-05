40c02c3fd9e18cad5888a834508578bade8df9a7
RyanZito <ryanthiago.dsantos@gmail.com>
2024-05-05 14:19:26 -0300

    v1.4

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,11 @@ var email = document.querySelector('input.input-email')
 var submit = document.querySelector('button.input-submit')
 
 submit.addEventListener('click', function(){
+    
+    if(userName.value === `` || email.value === ``){
+        alert(`Por favor, preencha os campos antes de envia-los. `)
+    }
+    else{
     alert(`Parabéns ${userName.value}!\nSeu email: ${email.value} foi cadastrado com sucesso!\nCaso os dados estejam incorretos, envie-os novamente.`)
+    }
 })
