3f4c92db62ff4fcfd43e4ac55090cb33855c2d1a
RyanZito <ryanthiago.dsantos@gmail.com>
2024-08-29 23:37:33 -0300

    v1.5

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,22 @@
 var body = document.body
 var button = document.querySelector('input.toggle')
-var container = document.querySelector('section.container')
-var header = document.querySelector('header.header')
+var container = document.querySelector('section.main_container')
+var header = document.querySelector('header.main_header')
 var products = document.querySelector('ol.products')
 var submit = document.querySelector('button.input-submit')
 
 button.addEventListener("click", function(){
     if(button.checked){
         body.classList = 'body_whiteTheme'
-        container.classList = 'container_whiteTheme'
-        header.classList = 'header_whiteTheme'
+        container.classList = 'main_container_whiteTheme'
+        header.classList = 'main_header_whiteTheme'
         products.classList = 'products_whiteTheme'
         submit.classList = 'input-submit_whiteTheme'
     }
     else{
         body.classList = 'body'
-        container.classList = 'container'
-        header.classList = 'header'
+        container.classList = 'main_container'
+        header.classList = 'main_header'
         products.classList = 'products'
         submit.classList = 'input-submit'
     }
